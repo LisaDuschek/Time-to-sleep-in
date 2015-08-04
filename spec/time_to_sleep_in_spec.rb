c5bd83aa69b285052523e73b48aeccc912106ec1
Vaughn Dice & Lisa Duschek <student@epicodus.com>
2015-08-04 11:01:12 -0700

    re-wording time_to_sleep_in_spec and adding .rspec config

--- spec/time_to_sleep_in_spec.rb
+++ spec/time_to_sleep_in_spec.rb
@@ -3,11 +3,11 @@ require('time_to_sleep_in')
 require('pry')
 
 describe('Time#time_to_sleep_in') do
-  it("takes a date and tells you what day of the week it is and if you can sleep in - sad") do
+  it("takes a date and tells you which day of the week it is and if you can sleep in - weekday") do
     expect((Time.new 2015, 8, 4).time_to_sleep_in).to(eq("It is Tuesday so you CANNOT sleep in!"))
   end
 
-  it("takes a date and tells you what day of the week it is and if you can sleep in - happy") do
+  it("takes a date and tells you which day of the week it is and if you can sleep in - weekend") do
     expect((Time.new 2015, 8, 2).time_to_sleep_in).to(eq("It is Sunday so you can sleep in!"))
   end
 end
