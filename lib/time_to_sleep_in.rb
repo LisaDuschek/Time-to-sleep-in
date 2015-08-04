ad79dcb9aea443888da719f4ca5d587b2b60cf00
Vaughn Dice & Lisa Duschek <student@epicodus.com>
2015-08-04 10:55:48 -0700

    finalized time_to_sleep_in implementation

--- lib/time_to_sleep_in.rb
+++ lib/time_to_sleep_in.rb
@@ -1,9 +1,39 @@
 class Time
   define_method(:time_to_sleep_in) do
+    message = "It is "
+
+    ## Our first approach:
+    # if self.monday?
+    #   message += "Monday"
+    # elsif self.tuesday?
+    #   message += "Tuesday"
+    # elsif self.wednesday?
+    #   message += "Wednesday"
+    # elsif self.thursday?
+    #   message += "Thursday"
+    # elsif self.friday?
+    #   message += "Friday"
+    # elsif self.saturday?
+    #   message += "Saturday"
+    # elsif self.sunday?
+    #   message += "Sunday"
+    # end
+
+    weekday_hash = Hash[0,'Sunday',
+                        1,'Monday',
+                        2,'Tuesday',
+                        3,'Wednesday',
+                        4,'Thursday',
+                        5,'Friday',
+                        6,'Saturday']
+
+    message += weekday_hash[self.wday]
+
     if self.saturday? || self.sunday?
-      true
+      message += " so you can sleep in!"
     else
-      false
+      message += " so you CANNOT sleep in!"
     end
+
   end
 end
--- spec/time_to_sleep_in_spec.rb
+++ spec/time_to_sleep_in_spec.rb
@@ -2,13 +2,12 @@ require('rspec')
 require('time_to_sleep_in')
 require('pry')
 
-
-describe('Time #time_to_sleep_in') do
-  it("takes a date and if weekday tells the user they can't sleep in") do
-    expect((Time.new(2015, 8, 4)).time_to_sleep_in()).to(eq(false))
+describe('Time#time_to_sleep_in') do
+  it("takes a date and tells you what day of the week it is and if you can sleep in - sad") do
+    expect((Time.new 2015, 8, 4).time_to_sleep_in).to(eq("It is Tuesday so you CANNOT sleep in!"))
   end
 
-  it("takes a date and if weekend tells the user they can sleep in") do
-    expect((Time.new(2015, 8, 2)).time_to_sleep_in()).to(eq(true))
+  it("takes a date and tells you what day of the week it is and if you can sleep in - happy") do
+    expect((Time.new 2015, 8, 2).time_to_sleep_in).to(eq("It is Sunday so you can sleep in!"))
   end
 end
